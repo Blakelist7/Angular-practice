c3deed441b3fb91d2278e6db80cf2c102abfa963
Blakelist7 <kanwal.88singh@gmail.com>
2021-03-19 22:50:58 +0530

    Practicing two way binding, directives like *ngIf, *ngFor,*ngModel etc..

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from "./hero";
+ export const HEROES: Hero[]= [
+     {id:10, name:'Mikasa'},
+     {id:11, name:'Erwin'},
+     {id:12, name:'Historia'},
+     {id:13, name:'Anne'},
+     {id:14, name:'Sasha'}
+ ];
